2a6556f2f456573f747e23ab1e5b723dbb973a9f
tan_l <tan_l@Ctrip.com>
2018-01-17 18:39:49 +0800

    update

---
+++ js/spa.shell.js
@@ -0,0 +1,34 @@
+/**
+ * jslint browser: true, continue: true,
+ * devel: true, indent: 2, maxerr: 50,
+ * newcap: true, nomen: true, plusplus: true,
+ * regexp: true, sloppy: true, vars: false,
+ * white: true
+ */
+/* global $, spa */
+
+spa.shell = (function () {
+    // BEGIN MODULE SCOPE VARIABLES
+    var
+        configMap = {
+            main_html: String () +
+            '<div class="spa-shell-head">' +
+            '    <div class="spa-shell-head-logo"></div>' +
+            '    <div class="spa-shell-head-acct"></div>' +
+            '    <div class="spa-shell-head-search"></div>' +
+            '</div>' +
+            '<!-- <div class="spa-shell-main spa-x-closed"> -->' +
+            '<div class="spa-shell-main">' +
+            '    <div class="spa-shell-main-nav"></div>' +
+            '    <div class="spa-shell-main-content"></div>' +
+            '</div>' +
+            '<div class="spa-shell-foot"></div>' +
+            '<div class="spa-shell-chat"></div>' +
+            '<div class="spa-shell-modal"></div>';
+
+        },
+        stateMap: { $container: null },
+        jqueryMap: {},
+        setJqueryMap, initModule;
+})
+
